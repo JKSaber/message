f186dac572a155fdc17f6461286fd09470e2871b
JKSaber <76680484+JKSaber@users.noreply.github.com>
2020-12-28 10:53:31 +0300

    app Message
    TCP Client and Server

---
+++ MessageClient/MessageClient/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Text;
+using System.IO;
+
+namespace MessageClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool check = false;
+            var message = "";
+            var repetition = "";
+
+            do
+            {
+                Console.WriteLine("\nВведите сообщение (не более 20 символов)");
+                message = Console.ReadLine();
+                if (message.Length > 20)
+                    {
+                    Console.WriteLine("В сообщении больше 20 символов!");
+                }
+                else
+                {
+                    check = true;
+                }
+            }
+            while (check == false);
+
+            check = false;
+            do
+            {
+                Console.WriteLine("\nВведите количество повторений (не более 100)");
+                repetition = Console.ReadLine();
+                if (int.Parse(repetition) > 100)
+                {
+                    Console.WriteLine("Повторений должно быть не больше 100!");
+                }
+                else
+                {
+                    check = true;
+                }
+            }
+            while (check == false);
+            
+        connection:
+            try
+            {
+                int count_repetition = int.Parse(repetition);
+
+                for (int i = 0; i < count_repetition; i++)
+                {
+                    TcpClient client = new TcpClient("127.0.0.1", 1302);
+                    string messageToSend = message;
+                    
+                    int byteCount = Encoding.ASCII.GetByteCount(messageToSend + 1);
+                    byte[] sendData = new byte[byteCount];
+                    sendData = Encoding.ASCII.GetBytes(messageToSend);
+                    NetworkStream stream = client.GetStream();
+                    stream.Write(sendData, 0, sendData.Length);
+                    Console.WriteLine("sending data to server...");
+
+                    StreamReader sr = new StreamReader(stream);
+                    string response = sr.ReadLine();
+                    Console.WriteLine(response);
+
+                    stream.Close();
+                    client.Close();
+                }
+
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("failed to connect...");
+                goto connection;
+            }
+
+
+        }
+    }
+}
---
+++ MessageServer/MessageServer/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Text;
+using System.IO;
+using System.Net.Sockets;
+using System.Net;
+using System.Threading;
+using Serilog;
+using System.Reflection;
+
+namespace MessageServer
+{
+    class Program
+    {
+        private static void ProcessClientRequests(object argument)
+        {
+            TcpClient client = (TcpClient)argument;
+            try
+            {
+                StreamReader sr = new StreamReader(client.GetStream());
+                StreamWriter sw = new StreamWriter(client.GetStream());
+                NetworkStream stream = client.GetStream();
+                byte[] buffer = new byte[1024];
+                stream.Read(buffer, 0, buffer.Length);
+                int recv = 0;
+                foreach (byte b in buffer)
+                {
+                    if (b != 0)
+                    {
+                        recv++;
+                    }
+                }
+                string request = Encoding.ASCII.GetString(buffer, 0, recv);
+                Console.WriteLine("request received: " + request);
+                byte[] asciiBytes = Encoding.ASCII.GetBytes(request);
+                int asciiBytesSum = 0;
+                foreach (byte b in asciiBytes)
+                {
+                    asciiBytesSum = asciiBytesSum + b;
+                }
+                string answer = String.Join("+", asciiBytes) + "=" + asciiBytesSum.ToString();
+               
+                sw.WriteLine(answer);
+                var pi = stream.GetType().GetProperty("Socket", BindingFlags.NonPublic | BindingFlags.Instance);
+                var socketIp = ((Socket)pi.GetValue(stream, null)).RemoteEndPoint.ToString();
+                Log.Information(socketIp + " " + request + " " + answer);
+                
+                sw.Flush();
+                sr.Close();
+                sw.Close();
+                client.Close();
+            }
+            catch (IOException)
+            {
+                Console.WriteLine("Problem with client communication. Exiting thread");
+            }
+            finally
+            {
+                if (client != null)
+                {
+                    client.Close();
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            Log.Logger = new LoggerConfiguration()
+                .WriteTo.File("consoleapp.log")
+                .CreateLogger();
+
+            TcpListener listener = null;
+                try
+                {
+                    listener = new TcpListener(System.Net.IPAddress.Any, 1302);
+                    listener.Start();
+                    while (true)
+                    {
+                        Console.WriteLine("Waiting for a connection");
+                        TcpClient client = listener.AcceptTcpClient();
+                        Console.WriteLine("Client accepted");
+                        Thread t = new Thread(ProcessClientRequests);
+                        t.Start(client);
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                }
+
+        }
+    }
+}
